9868a3d150cb4737679a62d13183e4100c078d3d
rcnuvas-code <rcnuvas@gmail.com>
2026-04-20 17:40:07 +0530

    Update data.tf

--- 2-eks/data.tf
+++ 2-eks/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "s3"
 
   config = {
-    bucket = "rcnuvas-infra-statefile-backup"
+    bucket = "rcnuvas-bucket-1"
     key    = "rcnuvas/1-network/terraform.tfstate"
     region = "ap-northeast-1"
   }
