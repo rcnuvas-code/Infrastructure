fb6ace721fa50824b35204adf161f4db9536802c
rcnuvas-code <rcnuvas@gmail.com>
2026-04-20 17:43:59 +0530

    Update backend.tf

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "rcnuvas-infra-statefile-backup"
+    bucket         = "rcnuvas-bucket-1"
     key            = "rcnuvas/1-network/terraform.tfstate"
     region         = "ap-northeast-1"
     dynamodb_table = "rcnuvas-terraform-locks"
